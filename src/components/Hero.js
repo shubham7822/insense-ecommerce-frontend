ec38504c6f4fcb31b678fad26617db08f86d7e97
shubham mansute <shubhammansute7822@gmail.com>
2023-11-02 06:45:44 +0530

    chaned tagline

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
     <Box w={[ "100%", "100%", "70%"]} >
             <Box w="100%" h="600px"  backgroundImage={`url(${incenseStickImg})`} backgroundSize="cover" bgPosition="50%, 75%" alt="bgImage"></Box>
     </Box>
-    <HeadingSection mainText={"Shiv krupa incense sticks!."} subText={"one stop solution for your all your daily religious needs."} />
+    <HeadingSection mainText={"Shiv krupa incense sticks!."} subText={"Experience the delightful aroma at your doorstep."} />
 </Flex>
   )
 }
